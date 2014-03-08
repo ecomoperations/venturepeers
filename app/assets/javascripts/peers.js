6853574846b95924c30f90f0f014ae1e766de976
Garrett Flanagan <ecomoperations@gmail.com>
2014-03-08 17:29:06 -0500

    pretty good tyling

--- app/assets/javascripts/peers.js
+++ app/assets/javascripts/peers.js
@@ -676,35 +676,3 @@
 // --------------------------Multi step form ----------------
 // ----------------------------------------------------------
 
-$(document).ready ->
-  navListItems = $("ul.setup-panel li a")
-  allWells = $(".setup-content")
-  allWells.hide()
-  navListItems.click (e) ->
-    e.preventDefault()
-    $target = $($(this).attr("href"))
-    $item = $(this).closest("li")
-    unless $item.hasClass("disabled")
-      navListItems.closest("li").removeClass "active"
-      $item.addClass "active"
-      allWells.hide()
-      $target.show()
-    return
-
-  $("ul.setup-panel li.active a").trigger "click"
-  
-  
-  $("#activate-step-2").on "click", (e) ->
-    $("ul.setup-panel li:eq(1)").removeClass "disabled"
-    $("ul.setup-panel li a[href=\"#step-2\"]").trigger "click"
-    $(this).remove()
-    return
-
-  $("#activate-step-3").on "click", (e) ->
-    $("ul.setup-panel li:eq(2)").removeClass "disabled"
-    $("ul.setup-panel li a[href=\"#step-3\"]").trigger "click"
-    $(this).remove()
-    return
-
-  return
-
